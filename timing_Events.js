c39ce51f24020567b5d7b73364a1f32cd8e3ccfa
kamilla preeti samuel <106230983+Preetisam@users.noreply.github.com>
2022-10-28 21:53:05 +0530

    Create timing_Events.js

---
+++ timing_Events.js
@@ -0,0 +1,10 @@
+function openBankAccount(){
+    let clear = setTimeout(() => {
+        console.log("id verification & photograph");
+    },2000);
+
+    clearTimeout(clear);
+}
+
+openBankAccount();
+console.log("sucessfully opened account");
