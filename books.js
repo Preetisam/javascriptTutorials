2aa9acf7470b42926e158052b91f01a428162942
kamilla preeti samuel <106230983+Preetisam@users.noreply.github.com>
2022-11-27 13:04:09 +0530

    Create books.js

---
+++ books.js
@@ -0,0 +1,20 @@
+Requirements: "1./In/our/mini/project/built/till/session3/let’s/add/custom/filters/as/mentioned/below.
+Get/books/with/given/title/.
+Get/books/after/given/published/date"
+
+1. In our mini project built till session3, let's add custom filters as mentioned below.
+Get books with given title.
+Get books after given published date.
+code
+
+2. In our mini project built till session3, let's add custom filters as mentioned below.
+Get books with given title.
+Get books after given published date.
+
+3. In our mini project built till session3, let's add custom filters as mentioned below.
+Get books with given title.
+Get books after given published date.
+
+4. In our mini project built till session3, let's add custom filters as mentioned below.
+Get books with given title.
+Get books after given published date.
